6af9db54c97886527c7128905b254e28f48cca5d
mahan-dob <mahanfazli6@gmail.com>
2026-02-15 17:34:03 +0000

    done

--- script.js
+++ script.js
@@ -1,46 +1,4 @@
 
-// function google(){
-//     alert('redy');
-//     window.location.href = "https://google.com";
-// }
-
-// setTimeout(google,3000)
-
-// let newWindow = window.open("https://google.com", "_blank");
-
-// function closeWin (){
-//     window.close(newWindow)
-// }
-
-// setTimeout(closeWin,5000)
-
-// window.scrollTo({
-//     top:1000
-// })
-
-// const nodeslen = document.body.childNodes.length;
-
-// for (let index = 0; index < nodeslen; index++) {
-//     console.log(document.body.childNodes[index]);
-// }
-
-
-// let isred=true;
-// function setColor (){
-//     if(isred){
-//         document.body.style.background="#ef0000";
-//         isred=false;
-//     }else{
-//         document.body.style.background="#00b";
-//         isred=true;
-//     }
-// }
-
-// setInterval(setColor,2000);
-
-// document.getElementById("test").style="    background: red;";
-
-
 let rows = document.querySelectorAll("table tbody tr");
 
 for (let i = 0; i < rows.length; i++) {
